f61fda05da203ca2eeca4ba9e8422ab64d937e4c
Paresh Barad <31455201+paresh-barad@users.noreply.github.com>
2022-09-05 17:49:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World! 123'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
